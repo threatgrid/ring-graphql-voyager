2eaaf58ae4ecc810e50646b4a485beb91f9bb091
Matthieu Sprunck (msprunck) <matthieu@sprunck.com>
2017-05-23 16:02:20 +0200

    Release 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject threatgrid/ring-graphql-voyager "0.1.0-SNAPSHOT"
+(defproject threatgrid/ring-graphql-voyager "0.1.0"
   :description "GraphQL Voyager for Ring apps"
   :url "https://github.com/threatgrid/ring-graphql-voyager"
   :license {:name "Eclipse Public License"
